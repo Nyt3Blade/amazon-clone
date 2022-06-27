9535875d791e73e3cdc206d4de552542860ab56c
Yash <yashyadav942004@gmail.com>
2022-06-27 20:02:30 +0530

    more changes

--- src/Product.js
+++ src/Product.js
@@ -2,21 +2,21 @@ import React from 'react'
 import "./Product.css";
 import GradeIcon from '@mui/icons-material/Grade';
 
-function Product() {
+function Product(title, image, price, rating) {
   return (
     <div className='product'>
         <div className='product__info'>
-            <p>The product info</p>
+            <p>{title}</p>
             <p className='product__price'> 
                 <small>$</small>
-                <strong>0.00</strong>
+                <strong>{price}</strong>
             </p>
             <div className='product__rating'>
-                <p>"</p><GradeIcon style={{ color: "rgb(255, 200, 0)" }} /><p>"</p>
+                <p>"</p>{Array(rating).fill().map((_,i)=> (<GradeIcon style={{ color: "rgb(255, 200, 0)" }} />))}<p>"</p>
             </div>
         </div>
         <img src="https://cdn.pixabay.com/photo/2017/05/15/17/43/cup-2315563__340.jpg" alt="coffeeMugImage"></img>
-        <button>Add To Basket</button>
+        <button className="product__button">Add To Basket</button>
     </div>
   )
 }
